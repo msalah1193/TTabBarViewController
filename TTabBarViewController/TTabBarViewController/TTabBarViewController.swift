721e49a64edf9c13369e4a27dd79d06839367f1f
Mohamed Salah <mohammed.salah1193@gmail.com>
2018-01-30 15:13:22 +0200

    Initial commit for Xcode project

---
+++ TTabBarViewController/TTabBarViewController/TTabBarViewController.swift
@@ -0,0 +1,65 @@
+//
+//  TTabBarViewController.swift
+//  TTabBarViewController
+//
+//  Created by Mohamed Salah on 1/30/18.
+//  Copyright © 2018 Mohamed Salah. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage{
+    //Draws the top indicator by making image with filling color
+    class func drawTabBarIndicator(color: UIColor, size: CGSize, onTop: Bool) -> UIImage {
+        let indicatorHeight = size.height / 30
+        let yPosition = onTop ? 0 : (size.height - indicatorHeight)
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        color.setFill()
+        UIRectFill(CGRect(x: 0, y: yPosition, width: size.width, height: indicatorHeight))
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image!
+    }
+}
+
+/**
+    Custom UITabBarController class which draw line indicator
+    with custom color above or below the selected tab bar item using
+    selectionIndicatorImage property on the UITabBar class
+ **/
+@IBDesignable class TTabBarViewController: UITabBarController{
+    /*
+        color of the indicator
+     */
+    @IBInspectable var indicatorColor: UIColor = UIColor()
+    
+    /*
+        determine if the indicator
+        will be drawn on top of bar items or not
+     */
+    @IBInspectable var onTopIndicator: Bool = true
+    
+    
+    //MARK:- View Controller Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Draw Indicator above the tab bar items
+        guard let numberOfTabs = tabBar.items?.count else {
+                return
+        }
+        
+        let numberOfTabsFloat = CGFloat(numberOfTabs)
+        let imageSize = CGSize(width: tabBar.frame.width / numberOfTabsFloat,
+                               height: tabBar.frame.height)
+        
+        
+        let indicatorImage = UIImage.drawTabBarIndicator(color: indicatorColor,
+                                                         size: imageSize,
+                                                         onTop: onTopIndicator)
+        self.tabBar.selectionIndicatorImage = indicatorImage
+    }
+    
+}
---
+++ TTabBarViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TTabBarViewController
+//
+//  Created by Mohamed Salah on 1/30/18.
+//  Copyright © 2018 Mohamed Salah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
